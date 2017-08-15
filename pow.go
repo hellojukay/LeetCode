45391acf5df499586cfcc837989159b0437406a6
lic <lic@didapinche.com>
2017-08-15 12:47:39 +0800

    add myPow

---
+++ pow.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+// func myPow(x float64, n int) float64 {
+// 	if n < 0 {
+// 		x = 1 / x
+// 		n = -n
+// 	}
+// 	if n == 0 {
+// 		return 1
+// 	}
+// 	return x * myPow(x, n-1)
+// }
+
+func myPow(x float64, n int) float64 {
+	if n < 0 {
+		x = 1 / x
+		n = -n
+	}
+	if n == 0 {
+		return 1
+	}
+	if n%2 == 0 {
+		return myPow(x*x, n/2)
+	}
+	return x * myPow(x, n-1)
+}
+func main() {
+	fmt.Print(myPow(2.0, 3))
+}
